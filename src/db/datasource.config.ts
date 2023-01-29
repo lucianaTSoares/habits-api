7f1f8076e755bdae9c9f9b39b3772bd1b044a520
lucianaTSoares <lucianat.s@hotmail.com>
2023-01-29 03:56:58 -0300

    build: new dependencies and datasource typeorm configuration

---
+++ src/db/datasource.config.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
